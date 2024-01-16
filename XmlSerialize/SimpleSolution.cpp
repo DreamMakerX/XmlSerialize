a0f8296176b49eb8a7e10d3476f9e3b7ae74fbae
congcong.zhang <zccbjut@163.com>
2024-01-16 14:19:53 +0800

    congcong.zhang 2024-01-16 upload XmlSerialize project

---
+++ XmlSerialize/SimpleSolution.cpp
@@ -0,0 +1,103 @@
+﻿#include "XmlSerializer.h"
+
+class Grade {
+public:
+    std::string courseName;
+    std::string teacherName;
+    int score;
+
+    void toXml(XMLElement* elem) const {
+        XmlSerializer::writeElement(elem, "CourseName", courseName);
+        XmlSerializer::writeElement(elem, "TeacherName", teacherName);
+        XmlSerializer::writeElement(elem, "Score", score);
+    }
+
+    void fromXml(XMLElement* elem) {
+        XmlSerializer::readElement(elem, "CourseName", courseName);
+        XmlSerializer::readElement(elem, "TeacherName", teacherName);
+        XmlSerializer::readElement(elem, "Score", score);
+    }
+};
+class Student {
+public:
+    std::string name;
+    int id;
+    std::vector<Grade> grades;
+
+    void toXml(XMLElement* elem) const {
+        XmlSerializer::writeElement(elem, "Name", name);
+        XmlSerializer::writeElement(elem, "Id", id);
+        XmlSerializer::writeElementObject(elem, "Grades", grades);
+    }
+
+    void fromXml(XMLElement* elem) {
+        XmlSerializer::readElement(elem, "Name", name);
+        XmlSerializer::readElement(elem, "Id", id);
+        XmlSerializer::readElementObject(elem, "Grades", grades);
+    }
+};
+
+std::string GetXmlString()
+{
+    return "<Root><Name>zcc</Name><Id>4078</Id><Grades><Item><CourseName>Math</CourseName><TeacherName>bo.wang</TeacherName><Score>95</Score></Item><Item><CourseName>Physics</CourseName><TeacherName>dingtai.tang</TeacherName><Score>100</Score></Item></Grades></Root>";
+/*XML文件格式
+<Root>
+    <Name>zcc</Name>
+    <Id>4078</Id>
+    <Grades>
+        <Item>
+            <CourseName>Math</CourseName>
+            <TeacherName>bo.wang</TeacherName>
+            <Score>95</Score>
+        </Item>
+        <Item>
+            <CourseName>Physics</CourseName>
+            <TeacherName>dingtai.tang</TeacherName>
+            <Score>100</Score>
+        </Item>
+    </Grades>
+</Root>
+*/
+}
+void PrintStudentObject(const Student& student)
+{
+    std::cout << "====================print student object start====================" << std::endl;
+    std::cout << "Deserialized Student:" << std::endl;
+    std::cout << "Name: " << student.name << std::endl;
+    std::cout << "ID: " << student.id << std::endl;
+    std::cout << "Grades:" << std::endl;
+    for (const Grade& grade : student.grades) {
+        std::cout << "Course Name: " << grade.courseName << std::endl;
+        std::cout << "Teacher Name: " << grade.teacherName << std::endl;
+        std::cout << "Score: " << grade.score << std::endl;
+        std::cout << std::endl;
+    }
+    std::cout << "====================print student object end======================" << std::endl;
+}
+void PrintXmlString(const std::string& xml)
+{
+    std::cout << "====================print xml string start========================" << std::endl;
+    std::cout << xml << std::endl;
+    std::cout << "====================print xml string end==========================" << std::endl;
+}
+
+void main() {
+
+    //执行反序列化
+    Student student;
+    XmlSerializer::Deserialize(GetXmlString(), student);
+
+    //遍历输出Student对象
+    PrintStudentObject(student);
+
+    //任意修改对象属性，以id为例
+    student.id = 363258;
+
+    //执行序列化
+    std::string xml = XmlSerializer::Serialize(student);
+
+    //输出序列化结果
+    PrintXmlString(xml);
+
+    std::cin.get();
+}
---
+++ XmlSerialize/XmlSerializer.h
@@ -0,0 +1,106 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include "tinyxml2.h"
+
+using namespace tinyxml2;
+
+class XmlSerializer {
+public:
+    template <typename T>
+    static void writeElement(XMLElement* parentElem, const std::string& elementName, const T& value) {
+        XMLElement* elem = parentElem->GetDocument()->NewElement(elementName.c_str());
+        elem->SetText(value);
+        parentElem->InsertEndChild(elem);
+    }
+
+    template <>
+    static void writeElement(XMLElement* parentElem, const std::string& elementName, const std::string& value) {
+        XMLElement* elem = parentElem->GetDocument()->NewElement(elementName.c_str());
+        elem->SetText(value.c_str());
+        parentElem->InsertEndChild(elem);
+    }
+
+    template <typename T>
+    static void readElement(XMLElement* parentElem, const std::string& elementName, T& value) {
+        XMLElement* elem = parentElem->FirstChildElement(elementName.c_str());
+        if (elem) {
+            value = std::stoi(elem->GetText());
+        }
+    }
+
+    template <>
+    static void readElement(XMLElement* parentElem, const std::string& elementName, std::string& value) {
+        XMLElement* elem = parentElem->FirstChildElement(elementName.c_str());
+        if (elem) {
+            value = elem->GetText();
+        }
+    }
+
+    template <typename T>
+    static void writeElementObject(XMLElement* parentElem, const std::string& elementName, const T& object) {
+        XMLElement* elem = parentElem->GetDocument()->NewElement(elementName.c_str());
+        object.toXml(elem);
+        parentElem->InsertEndChild(elem);
+    }
+
+    template <typename T>
+    static void writeElementObject(XMLElement* parentElem, const std::string& elementName, const std::vector<T>& objects) {
+        XMLElement* vectorElem = parentElem->GetDocument()->NewElement(elementName.c_str());
+        for (const auto& object : objects) {
+            writeElementObject(vectorElem, elementName + "Item", object);
+        }
+        parentElem->InsertEndChild(vectorElem);
+    }
+
+    template <typename T>
+    static void readElementObject(XMLElement* parentElem, const std::string& elementName, T& object, bool array = false) {
+        XMLElement* elem;
+        if (array)
+        {
+            elem = parentElem;
+        } 
+        else
+        {
+           elem = parentElem->FirstChildElement(elementName.c_str());
+        }
+        if (elem) {
+            object.fromXml(elem);
+        }
+    }
+
+    template <typename T>
+    static void readElementObject(XMLElement* parentElem, const std::string& elementName, std::vector<T>& objects) {
+       XMLElement* objectElem = parentElem->FirstChildElement(elementName.c_str());
+       for (XMLElement* elem = objectElem->FirstChildElement(); elem; elem = elem->NextSiblingElement())
+       {
+           T object;
+           readElementObject(elem, elem->Name(), object, true);
+           objects.push_back(object);
+       }
+    }
+
+    template <typename T>
+    static std::string Serialize(const T& object) {
+        XMLDocument doc;
+        XMLElement* rootElem = doc.NewElement("Root");
+        object.toXml(rootElem);
+        doc.InsertEndChild(rootElem);
+
+        XMLPrinter printer;
+        doc.Accept(&printer);
+
+        return printer.CStr();
+    }
+
+    template <typename T>
+    static void Deserialize(const std::string& xml, T& object) {
+        XMLDocument doc;
+        doc.Parse(xml.c_str());
+
+        XMLElement* rootElem = doc.RootElement();
+        if (rootElem) {
+            object.fromXml(rootElem);
+        }
+    }
+};
